07c631f1f76861ee5c7b2958c10069c4b9e42aa2
= <=>
2025-08-06 18:57:30 -0500

    Cambios en vistas

--- server.js
+++ server.js
@@ -1,8 +1,18 @@
 import express from "express"
 import mysql from "mysql2/promise"
+import path from "path";
+import { fileURLToPath } from "url";
 
 const app = express()
 const port = 3000
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use("/fontawesome", express.static(path.join(__dirname, "node_modules", "@fortawesome", "fontawesome-free")));
+
 
 async function main() {
     const connection = await mysql.createConnection({
@@ -24,9 +34,9 @@ async function main() {
         }
     });
 
-    app.get('/', (req, res) => {
-        send()
-    })
+    app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "public", "index.html"));
+  });
 
 
     app.listen(port, () => {
